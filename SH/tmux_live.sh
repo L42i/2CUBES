2a7b2ffc9da1da0fdbba4515a57c8839147a3628
anwaldt <hvc@gatech.edu>
2024-10-04 15:41:34 -0400

    Better - working with synth!

--- SH/tmux_live.sh
+++ SH/tmux_live.sh
@@ -1,13 +1,13 @@
 tmux new-session -d -s 2CUBES
 
 
-tmux send-keys -t 2CUBES 'jackd -a a -d alsa -d hw:KTUSB -p -r 48000 -p 256 -n 2' C-m
+tmux send-keys -t 2CUBES 'jackd -a a -d alsa -d hw:EVO16 -p -r 48000 -p 256 -n 2' C-m
 
 
 sleep 3
 
 tmux split-window -h -t 2CUBES
-tmux send-keys -t 2CUBES 'scide ../SC/2_Cubes.sc' C-m
+tmux send-keys -t 2CUBES 'scide ../SC/2_Cubes.scd' C-m
 
 tmux split-window -h -t 2CUBES
 tmux send-keys -t 2CUBES 'iem-plugin-allradecoder -loadSettings ../setup/Rich-133B_28-Speakers.json' C-m
