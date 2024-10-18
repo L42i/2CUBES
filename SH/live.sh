4de0d35e4efe6b99aa5205832395c59597594c23
l42i <hvc@gatech.edu>
2024-10-17 21:25:39 -0400

    Absolute Euler angles!

---
+++ SH/live.sh
@@ -0,0 +1,30 @@
+#INTERFACE=EVO16
+INTERFACE=Gen
+
+tmux new-session -d -s 2CUBES
+
+tmux send-keys -t 2CUBES 'jackd -a a -d alsa -d hw:'$INTERFACE' -p -r 48000 -p 256 -n 2' C-m
+
+sleep 3
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'scide ../SC/2_Cubes.scd' C-m
+
+tmux split-window -h -t 2CUBES
+#tmux send-keys -t 2CUBES 'iem-plugin-allradecoder -loadSettings ../setup/Rich-133B_28-Speakers.json' C-m
+tmux send-keys -t 2CUBES 'iem-plugin-binauraldecoder' C-m
+
+sleep 5
+
+tmux split-window -h -t 2CUBES
+#tmux send-keys -t 2CUBES 'aj-snapshot -x -d ../setup/Rich133B.snap' C-m
+tmux send-keys -t 2CUBES 'aj-snapshot -x -d ../setup/binaural.snap' C-m
+
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'qjackctl' C-m
+
+tmux select-layout -t 2CUBES tiled
+
+tmux attach-session -t 2CUBES
+
