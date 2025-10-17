1ae98042e556a1512fb237f6a1c2dc1848e034f8
anwaldt <contact@hvc.berlin>
2025-10-17 16:15:09 -0400

    Sigma!

---
+++ SH/live_LAC25.sh
@@ -0,0 +1,33 @@
+#INTERFACE=EVO16
+INTERFACE=U2A
+
+
+# sudo ~/Desktop/cpu-set-all.sh -g performance
+
+
+tmux new-session -d -s 2CUBES
+
+tmux send-keys -t 2CUBES 'jackd -P 95 -a a -d alsa -d hw:'$INTERFACE' -p -r 48000 -p 256 -n 2' C-m
+
+sleep 5
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'scide ../SC/2_Cubes.scd' C-m
+
+sleep 5
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'aj-snapshot -x -d ../setup/ICMC25.snap' C-m
+
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'reaper' C-m
+
+tmux split-window -h -t 2CUBES
+tmux send-keys -t 2CUBES 'jacktrip -C 10.11.12.100 -n 32 -J TO_SPEAKERS' C-m
+
+
+tmux select-layout -t 2CUBES tiled
+
+tmux attach-session -t 2CUBES
+
